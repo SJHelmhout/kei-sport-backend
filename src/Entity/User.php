36100c37c3129196644966481dedc00582731e7e
SJHelmhout <54943946+SJHelmhout@users.noreply.github.com>
2021-12-20 11:41:03 +0100

    Test deploy #2

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -28,6 +28,8 @@ use Symfony\Component\Serializer\Annotation\Groups;
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     //TODO: Velden die niet opgehaald mogen worden bij GET-requests niet meegeven.
+
+    //HOI
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
